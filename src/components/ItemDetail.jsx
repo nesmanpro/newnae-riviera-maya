15ec90ade98d63350288713ee88b63a0f3d58b29
nesmanpro <nesmanpro@gmail.com>
2023-11-20 13:05:40 +0100

    preparando json

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,16 +1,23 @@
 import { Banda } from "./Banda";
 import { BannerBot } from "./BannerBot";
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+
 
 export const ItemDetail = ({ item }) => {
 
-    const iconKeys = Object.keys(item.extras.text).slice(0, 6);
+    const firstSix = Object.keys(item.extras.text).slice(0, 6);
+    const secondSix = Object.keys(item.extras.text).slice(6, 12);
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY
+    })
 
     return (
         <div>
 
             {/* 1ra Seccion imagen principal */}
 
-            <div className="h-[55vh] overflow-hidden">
+            <div className="h-[65vh] overflow-hidden">
                 <img className="w-full bottom-[-200px]" src={item.img.img2} alt={item.subtitle} />
             </div>
 
@@ -20,13 +27,14 @@ export const ItemDetail = ({ item }) => {
                 <div className="bg-azulOscuro p-12 text-arenaClaro pl-20">
 
                     {/* Izquierda */}
+
                     <div className="border-b-1 border-azulClaro pb-5">
                         <h1 className="font-bebas text-6xl tracking-wide pb-5">{item.title}</h1>
                         <p className="leading-tight">{item.description.alojamiento}</p>
                     </div>
 
                     <div className="text-arenaclaro mt-5 grid grid-cols-2 gap-2">
-                        {iconKeys.map((key, index) => (
+                        {firstSix.map((key, index) => (
                             <div className="flex gap-3 mb-5" key={index}>
                                 <div dangerouslySetInnerHTML={{ __html: item.extras.icon[key] }} />
                                 <h4>{item.extras.text[key]}</h4>
@@ -37,6 +45,7 @@ export const ItemDetail = ({ item }) => {
                 </div>
 
                 {/* Derecha */}
+
                 <div>
                     <img className="h-full w-full object-cover" src={item.img.img1} alt={item.subtitle} />
                 </div>
@@ -49,15 +58,22 @@ export const ItemDetail = ({ item }) => {
                 <p className="mt-7">{item.description.acerca}</p>
             </div>
 
-            {/* 4ta Seccion e imagenes */}
+            {/* 4ta Seccion Imagenes */}
+
             <div className="grid grid-cols-3 w-full ">
                 <img src={item.img.img3} alt={item.subtitle} />
                 <img src={item.img.img4} alt={item.description.acerca} />
                 <img src={item.img.img5} alt={item.description.alojamiento} />
             </div>
-            <div>
-                <div className="grid grid-cols-3 p-5">
-                    <div className="col-span-2 p-10">
+
+            {/* 5ta Seccion Info y Mapa */}
+
+            <div className="my-16">
+                <div className="grid grid-cols-3 mx-20 my-10 gap-10">
+
+                    {/* Info */}
+
+                    <div className="col-span-2">
                         <div className="flex gap-5 text-3xl text-azulOscuro border-b-1 border-azulClaro pb-2 mb-5">
                             <h3 className="">{item.details.rooms}</h3>
                             <h3>·</h3>
@@ -67,15 +83,62 @@ export const ItemDetail = ({ item }) => {
                         </div>
                         <p>{item.description.acerca}</p>
                     </div>
-                    <div className="p-10">
+
+
+                    <div className="">
                         <div className="flex gap-5 text-3xl text-azulOscuro border-b-1 border-azulClaro pb-2 mb-5">
                             <h3 className="">Acerca de {item.details.area}</h3>
                         </div>
-                        <p>{item.description.acerca}</p>
+                        <p>{item.description.acceso}</p>
                     </div>
+
+                </div>
+                <div className="grid grid-cols-3 mx-20 my-10 gap-10">
+                    {/* mapa */}
+
+                    <div className="col-span-2">
+                        {isLoaded ?
+                            (<GoogleMap
+                                center={{
+                                    lat: item.details.location.lat,
+                                    lng: item.details.location.lng
+                                }}
+                                zoom={18}
+                                mapContainerStyle={{
+                                    width: '100%',
+                                    height: '100%'
+                                }}
+                            >
+                            </GoogleMap>) : null}
+                    </div>
+
+
+                    <div className="">
+                        <div className="flex gap-5 text-3xl text-azulOscuro border-b-1 border-azulClaro pb-2 mb-5">
+                            <h3 className="">otros servicios</h3>
+                        </div>
+                        <div className="text-arenaclaro mt-5 gap-2">
+                            {secondSix.map((key, index) => (
+                                <div className="flex gap-3 mb-5" key={index}>
+                                    <div dangerouslySetInnerHTML={{ __html: item.extras.icon[key] }} />
+                                    <h4>{item.extras.text[key]}</h4>
+                                </div>
+                            ))}
+                        </div>
+
+
+                    </div>
+
                 </div>
 
+            </div>
 
+            {/* 6ta Imagenes */}
+
+            <div className="grid grid-cols-3 w-full ">
+                <img src={item.img.img6} alt={item.subtitle} />
+                <img src={item.img.img7} alt={item.description.acerca} />
+                <img src={item.img.img8} alt={item.description.alojamiento} />
             </div>
             <Banda />
             <BannerBot />
