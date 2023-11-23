e7f8a1043f45cc926f557a673442a6e9bf269f35
nesmanpro <nesmanpro@gmail.com>
2023-11-23 13:21:06 +0100

    improving home 2

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -35,7 +35,7 @@ export const ItemList = ({ productos }) => {
                             onTouchStart={() => setHoveredIndex(index)}
                             onMouseEnter={() => setHoveredIndex(index)}
                             onMouseLeave={() => setHoveredIndex(null)}
-                            className={` border-azulClaro w-[80vw] 2xl:w-[50vw]  border-b-1 last:border-none hover:w-full px-20 py-20 sm:py-6 sm:pb-10 flex justify-center gap-7 items-center bg-center bg-cover transition-height ease-in-out h-32 hover:h-96 hover:border-none hover:shadow-inner-2xl`}
+                            className={`select-none border-azulClaro w-[80vw] 2xl:w-[50vw]  border-b-1 last:border-none hover:w-full px-20 py-20 sm:py-6 sm:pb-10 flex justify-center gap-7 items-center bg-center bg-cover transition-height ease-in-out h-32 hover:h-96 hover:border-none hover:shadow-inner-2xl`}
 
                             style={{
                                 backgroundImage: `url(${hoveredIndex === index ? prod.img.img1 : 'transparent'})`,
